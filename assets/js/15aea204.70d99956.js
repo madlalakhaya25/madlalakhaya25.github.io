ba60b0469c39b80d391742323d71769795a4f247
madlalakhaya25 <144236210+madlalakhaya25@users.noreply.github.com>
2024-04-10 11:43:54 +0200

    Deploy website - based on 32dbf41623c3e15d19a5e16172a0064c2680888a

--- assets/js/15aea204.70d99956.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvela_documentation=self.webpackChunkvela_documentation||[]).push([[534],{2945:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/acecf23e.7ecc608e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvela_documentation=self.webpackChunkvela_documentation||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
